73e4345a7db60eecbb3b6bfc5660a9c2ac1a51b9
Marcel Martino <marcelmartino2053@gmail.com>
2023-01-26 21:44:19 +0100

    add series counter add basic HTML structure

--- src/ts/components/PageComponent/PageComponent.ts
+++ src/ts/components/PageComponent/PageComponent.ts
@@ -1,9 +1,28 @@
 import Component from "../Component/Component";
-
+import series from "../../data/series";
+import { type Series } from "../../types/types";
 export default class PageComponent extends Component {
-  title: string;
+  series;
+  watchedSeriesCount: number;
+  pendingSeriesCount: number;
+
   constructor() {
     super(document.body, "container");
+    this.series = series;
+    this.getWatchedSeriesCount(this.series);
+  }
+
+  public getWatchedSeriesCount(array: Series) {
+    let count = 0;
+    // eslint-disable-next-line @typescript-eslint/prefer-for-of
+    for (let i = 0; i < array.length; i++) {
+      if (array[i].isWatched) {
+        count++;
+      }
+    }
+
+    this.pendingSeriesCount = 5 - count;
+    this.watchedSeriesCount = count;
   }
 
   public render() {
@@ -15,10 +34,20 @@ export default class PageComponent extends Component {
     <main class="main-content">
       <h2 class="main-content__title">Series list</h2>
       </main>
-      <section class= list">
-      </section>
-      <section class="list">
-      </section>
+        <section class= "list">
+        <h3 class="list__title">Pending series</h3>
+        <span class="list__info">You have ${this.pendingSeriesCount} series pending to watch</span>
+          <ul class= "series">
+
+          </ul>  
+        </section>
+        <section class="list">
+        <h3 class="list__title">Watched series</h3>
+        <span class="list__info">You have watched ${this.watchedSeriesCount} series</span>
+          <ul class= "series">
+
+          </ul>  
+        </section>
     `;
   }
 }
